6413fa55203cead38ab614132109744e707ac0c4
Xichao Chen <chenxichao@tamu.edu>
2018-01-26 06:42:48 +0000

    restful

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+//db init
+mongoose.connect('mongodb://localhost/yelp_camp');
 
-var campgrounds = [
-    {
-        name: 'college station',
-        image: 'https://photos.zillowstatic.com/p_e/ISukbrerb7ht9t0000000000.jpg'
-    },
-    {
-        name: 'byran',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Downtown-bryan2.jpg'
-    }
-    ];
+var campSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+var campground = mongoose.model('Campground', campSchema);
+
+// campground.create({
+    // name: 'byran',
+    // image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Downtown-bryan2.jpg'
+// }, function(err, campground){
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log('new campground created');
+//         console.log(campground);
+//     }
+// })
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}) );//
@@ -22,8 +34,15 @@ app.get('/', function(req, res){
     res.render('landing');
 });
 app.get('/campgrounds', function(req, res){
+    campground.find({}, function(err, campgrounds){
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.render('campgrounds', {campgrounds: campgrounds} );
+        }
+    })
     
-    res.render('campgrounds', {campgrounds: campgrounds} );
 });
 app.post('/campgrounds', function(req, res){
    // res.send('hit the post');
@@ -34,12 +53,31 @@ app.post('/campgrounds', function(req, res){
         name: name, image: image
     };  // init object use {}      
     //body parser get from body, the data is in json, with body parser, we can get name by req.body.name
-    campgrounds.push(newCamp);
-    res.redirect('/campgrounds');
+    campground.create(newCamp, function(err, newCampground){
+        if(err) {
+            console.log(err);
+        }
+        else
+        {
+            //console.log(newCampground);
+             res.redirect('/campgrounds');
+        }
+    })
+   
     //redirect to compground page,
 })
 app.get('/campgrounds/new', function(req, res) {
     res.render('new');
+})
+app.get('/campgrounds/:id', function(req, res) {
+    campground.findById(req.params.id, function(err, foundCamp){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.render('show', {campground: foundCamp});
+        }
+    });
 })
 //listen
 app.listen(process.env.PORT, process.env.IP, function(){
