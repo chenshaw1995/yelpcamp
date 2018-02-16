e3fa8d8193865020ff22fc6a832496f00c347659
Xichao Chen <chenxichao@tamu.edu>
2018-02-16 19:04:43 +0000

    v9

--- app.js
+++ app.js
@@ -1,83 +1,67 @@
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDb          = require("./seeds"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local")
+    
+var commentRoutes       = require("./routes/comments"),
+    campgroundRoutes    = require("./routes/campgrounds"),
+    indexRoutes         = require("./routes/index")
+    
+// seedDb();
 
 //db init
 mongoose.connect('mongodb://localhost/yelp_camp');
 
-var campSchema = new mongoose.Schema({
-    name: String,
-    image: String
-})
-
-var campground = mongoose.model('Campground', campSchema);
-
-// campground.create({
-    // name: 'byran',
-    // image: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Downtown-bryan2.jpg'
-// }, function(err, campground){
-//     if(err){
-//         console.log(err);
-//     }
-//     else{
-//         console.log('new campground created');
-//         console.log(campground);
-//     }
-// })
-
+app.use(express.static(__dirname + "/public"))// dirname safer to use public 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}) );//
 
+//================
+// passport config
+//================
+app.use(require("express-session")( {
+    secret: "There is someone HAVE A FEELING TO HUANGQIN ",
+    resave: false,
+    saveUninitialized: false
+} ));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//middleware!!!
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+})
+
 //routes
-app.get('/', function(req, res){
-    res.render('landing');
-});
-app.get('/campgrounds', function(req, res){
-    campground.find({}, function(err, campgrounds){
-        if(err) {
-            console.log(err);
-        }
-        else {
-            res.render('campgrounds', {campgrounds: campgrounds} );
-        }
-    })
-    
-});
-app.post('/campgrounds', function(req, res){
-   // res.send('hit the post');
-    // console.log(typeof req.body.name);
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCamp = {
-        name: name, image: image
-    };  // init object use {}      
-    //body parser get from body, the data is in json, with body parser, we can get name by req.body.name
-    campground.create(newCamp, function(err, newCampground){
-        if(err) {
-            console.log(err);
-        }
-        else
-        {
-            //console.log(newCampground);
-             res.redirect('/campgrounds');
-        }
-    })
-   
-    //redirect to compground page,
-})
-app.get('/campgrounds/new', function(req, res) {
-    res.render('new');
-})
-app.get('/campgrounds/:id', function(req, res) {
-    campground.findById(req.params.id, function(err, foundCamp){
-        if(err){
-            console.log(err);
-        }
-        else {
-            res.render('show', {campground: foundCamp});
-        }
-    });
+
+// ================================
+// campgrounds
+// ================================
+app.use(indexRoutes);
+app.use("/campgrounds/:id/comments" ,commentRoutes);
+app.use("/campgrounds", campgroundRoutes);
+
+//=====================================================
+//comment
+//=====================================================
+
+// ======
+// auth routes
+// ======
+
+app.get("*",  function(req, res) {
+    res.send("404 NOT FOUND")
 })
 //listen
 app.listen(process.env.PORT, process.env.IP, function(){
